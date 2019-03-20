fcfecfa1ca8719201fd2faf5322124eafa1ecfc6
jeyagandhi <gandigit@gmail.com>
2019-03-20 12:42:52 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ordproc-ui
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  //catalogServiceURL: 'http://9.204.168.81:31494/' ,
+  orderServiceURL: 'http://9.204.168.81:32381/',
+  catalogServiceURL: 'http://localhost:8082/' 
+
+  //endpoint = 'http://localhost:8082/';
+    //endpoint = 'http://localhost:8081/';
+};
